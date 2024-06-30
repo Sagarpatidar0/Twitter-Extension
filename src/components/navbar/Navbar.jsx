283800eb1485849068b4b730e12d5826fc28d49d
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-06-30 18:51:41 +0530

    Add url to github svg

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="flex h-8 relative justify-center items-center bg-gray-700" id='navbar'>
       <div className="font-bold text-sm">SmartTweet</div>
       <div className="absolute right-0">
-        <img className='w-8 p-1 cursor-pointer' src="images/github-mark-white.png" alt="" />
+        <img onClick={() => {window.open('https://github.com/Sagarpatidar0/Twitter-Extension')}} className='w-8 p-1 cursor-pointer' src="images/github-mark-white.png" alt="" />
       </div>
     </div>
   </>
